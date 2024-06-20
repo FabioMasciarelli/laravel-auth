497a336cf48aad912c3ec9d610f74c05163ae6bc
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-06-20 17:17:00 +0200

    add: destroy method

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.admin')
 
 @section('content')
+
+    @if (session('message'))
+        <div class="alert alert-danger">
+            {{ session('message') }}
+        </div>
+    @endif
+
     <table class="table">
         <thead>
             <tr>
@@ -16,9 +23,19 @@
                     <th scope="row">{{ $project->title }}</th>
                     <td>{{ $project->readme }}</td>
                     <td>{{ $project->languages }}</td>
-                    <td>
-                        <a href="{{ route('admin.projects.show', ['project' => $project->slug]) }}" class="btn btn-info">Più dettagli</a>
-                        <a href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}" class="btn btn-warning">Modifica</a>
+                    <td class="d-flex gap-3">
+                        <a href="{{ route('admin.projects.show', ['project' => $project->slug]) }}"
+                            class="btn btn-success">Più Dettagli</a>
+                        <a href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}"
+                            class="btn btn-warning">Modifica</a>
+
+                        <form action="{{ route('admin.projects.destroy', ['project' => $project->slug]) }}" method="POST">
+
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Elimina</button>
+
+                        </form>
                     </td>
                 </tr>
             @endforeach
